cc5a594568152514affa0a41a84bacde1fc0eaa6
L3XxXa <alexkkk@inbox.ru>
2023-06-18 14:44:01 +0700

    Default pages init

---
+++ src/pages/default/DefaultPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./DefaultPage.css"
+const DefaultPage = () => {
+  return (
+    <div className="Default">
+
+    </div>
+  );
+};
+
+export default DefaultPage;
