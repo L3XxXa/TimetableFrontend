1f996ee23236b087f4c29c0e36f56534e6d4614a
L3XxXa <alexkkk@inbox.ru>
2023-06-19 14:24:22 +0700

    Default page need to fix styles

---
+++ src/components/buttons/logged_default_page_buttons/PersonalTimetableButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './PersonalTimetableButton.css'
+
+const PersonalTimetableButton = () => {
+  return (
+    <div>
+      <button className="PersonalTimetableButton"></button>
+    </div>
+  );
+};
+
+export default PersonalTimetableButton;
--- src/pages/default_logged_page/DefaultLoggedPage.jsx
+++ src/pages/default_logged_page/DefaultLoggedPage.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import FacultyTimetableButton from "../../components/buttons/logged_default_page_buttons/FacultyTimetableButton";
+import TeacherTimetableButton from "../../components/buttons/logged_default_page_buttons/TeacherTimetableButton";
+import PersonalTimetableButton from "../../components/buttons/logged_default_page_buttons/PersonalTimetableButton";
 
 const DefaultLoggedPage = () => {
   return (
-    <div>
-      
+    <div className="DefaultPage">
+      <div className="container">
+        <FacultyTimetableButton   />
+        <h1 className="h">Расписание</h1>
+        <TeacherTimetableButton   />
+        <PersonalTimetableButton/>
+      </div>
     </div>
   );
 };
