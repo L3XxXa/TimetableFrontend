ee65d1aa68277c3ecffe2545d748d55f28501c6b
L3XxXa <alexkkk@inbox.ru>
2023-06-19 14:14:44 +0700

    Navigating to default page after login

---
+++ src/components/buttons/logged_default_page_buttons/FacultyTimetableButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./FTB.css"
+
+
+const FacultyTimetableButton = () => {
+    return (
+        <div>
+            <button className="FTB" > </button>
+        </div>
+    );
+};
+
+export default FacultyTimetableButton;
