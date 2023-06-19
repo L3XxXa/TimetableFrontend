3ea4e4546df6c174f612e9b88cf9d5091f27dfa6
Shreksatronik <a.ordina@g.nsu.ru>
2023-06-19 18:02:33 +0700

    addFacultyPage

---
+++ src/components/buttons/AddSpecialityButton/AddSpecialityButton.jsx
@@ -0,0 +1,50 @@
+import React,{useState} from 'react';
+import './AddSpecialityButton.css'
+import ReactDOM from "react-dom";
+import CustomInputForAddFaculty from "../../inputs/CustomInputForAddFaculty";
+
+const AddSpecialityButton = () => {
+    const [show, setShow] = useState(false);
+    const [show2, setShow2] = useState(true);
+    const handleOpen = () => {
+        setShow(!show);
+    };
+
+    const handleOpen2 = () => {
+        setShow2(!show2);
+    };
+    return(
+        <div>
+            <h1 className="label">Добавить специализацию
+                <div className="accordian-header" onClick={handleOpen}>
+                <div className="sign">{show ? '-' : '+'}
+                </div>
+            </div>
+            </h1>
+            {show && (
+                <div className="accordian-body">
+                    <CustomInputForAddFaculty/>
+                    <h1 className="label">Добавить специализацию
+                        <div className="accordian-header2" onClick={handleOpen2}>
+                            <div className="sign">{show2 ? '-' : '+'}
+                            </div>
+                        </div>
+                    </h1>
+                    {show2 && (
+                        <div className="accordian-body2">
+                            <CustomInputForAddFaculty placeholder={"Введите название факультета"} />
+                        </div>
+                    )}
+                </div>
+            )}
+
+        </div>
+
+    );
+
+    const rootElement = document.getElementById("root");
+    ReactDOM.render(<AddSpecialityButton />, rootElement);
+
+};
+
+export default AddSpecialityButton;
---
+++ src/pages/AddFacultyPage/AddFacultyPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './AddFacultyPage.css'
+import AddFacultyForm from "../../components/forms/AddFacultyForm/AddFacultyForm";
+
+const AddFacultyPage = () => {
+    return (
+        <div className="AddFaculty">
+            <AddFacultyForm/>
+        </div>
+    );
+};
+
+export default AddFacultyPage;
