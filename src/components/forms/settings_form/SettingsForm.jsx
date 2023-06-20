07d6a8544a005bf0c679648211c738466bfeb41d
L3XxXa <alexkkk@inbox.ru>
2023-06-20 16:54:30 +0700

    sending this task to nastya

---
+++ src/components/forms/settings_form/SettingsForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SettingsForm = () => {
+  return (
+    <div className="settings__form__container">
+      <h1> this is settings</h1>
+    </div>
+  );
+};
+
+export default SettingsForm;
