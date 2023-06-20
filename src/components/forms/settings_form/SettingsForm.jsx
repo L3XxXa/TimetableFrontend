4b5cb4380c013f0ac37aad1986cddf80b8c5b48e
L3XxXa <alexkkk@inbox.ru>
2023-06-20 19:32:33 +0700

    Logic for buttons

--- src/components/forms/settings_form/SettingsForm.jsx
+++ src/components/forms/settings_form/SettingsForm.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
-import CustomInputForLogin from "../../inputs/CustomInputForLogin";
 import "./SettingsForm.css"
 import CustomInputForSettings from "../../inputs/CustomInputForSettings";
 import CustomInputForSettings2 from "../../inputs/CustomInputForSettings2";
-import LoginButton from "../../buttons/login_button/LoginButton";
 import AddUserButtonSettings from "../../buttons/ButtonsForSettings/AddUserButtonSettings";
 import AddFacultyButtonSettings from "../../buttons/ButtonsForSettings/AddFacultyButtonSettings";
 import ExitButtonSettings from "../../buttons/ButtonsForSettings/ExitButtonSettings";
 import GenerateButtonSettings from "../../buttons/ButtonsForSettings/GenerateButtonSettings";
 import cookies from "../../../cookies/Cookies";
+import {useNavigate} from "react-router-dom";
+
 const SettingsForm = () => {
+  const navigate = useNavigate()
+  function openAddUser(){
+    navigate('/addUser')
+  }
+  function openAddFaculty(){
+    navigate('/addFaculty')
+  }
+
+  function logout(){
+    cookies.addCookies('role', "")
+    cookies.addCookies('login', "")
+    cookies.addCookies('accessToken', '')
+    cookies.addCookies('refreshToken', '')
+    cookies.addCookies('type', '')
+    navigate('/')
+  }
+
   return (
       <div className="settings-form-container">
         <label className="label1">Имя пользователя</label>
@@ -17,16 +34,16 @@ const SettingsForm = () => {
           <label className="label2">Роль</label>
           <CustomInputForSettings2 role={cookies.getCookies("role")}/>
           <div className="add-user-button-button">
-              <AddUserButtonSettings text={"Добавить пользователя"} />
+              <AddUserButtonSettings onClick={openAddUser} text={"Добавить пользователя"} />
           </div>
           <div className="add-faculty-button-button">
-              <AddFacultyButtonSettings text={"Добавить факультет"} />
+              <AddFacultyButtonSettings onClick={openAddFaculty} text={"Добавить факультет"} />
           </div>
           <div className="generate-button-button">
               <GenerateButtonSettings text={"Сгенерировать расписание"} />
           </div>
           <div className="exit-button-button">
-              <ExitButtonSettings text={"Выйти"} />
+              <ExitButtonSettings onClick={logout} text={"Выйти"} />
           </div>
 
       </div>
