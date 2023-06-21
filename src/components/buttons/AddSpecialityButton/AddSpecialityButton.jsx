4824254b9255f6ab009c7818d36523c15475547b
L3XxXa <alexkkk@inbox.ru>
2023-06-21 13:42:52 +0700

    Add specialization completed

--- src/components/buttons/AddSpecialityButton/AddSpecialityButton.jsx
+++ src/components/buttons/AddSpecialityButton/AddSpecialityButton.jsx
@@ -1,11 +1,12 @@
 import React,{useState} from 'react';
 import './AddSpecialityButton.css'
-import ReactDOM from "react-dom";
 import CustomInputForAddFaculty from "../../inputs/CustomInputForAddFaculty";
 
-const AddSpecialityButton = () => {
+const AddSpecialityButton = (props) => {
     const [show, setShow] = useState(false);
     const [show2, setShow2] = useState(false);
+    const [spec1, setSpec1] = useState("")
+    const [spec2, setSpec2] = useState("")
     const handleOpen = () => {
         setShow(!show);
     };
@@ -13,6 +14,22 @@ const AddSpecialityButton = () => {
     const handleOpen2 = () => {
         setShow2(!show2);
     };
+
+    function sendData(){
+        let data
+        if (spec2===""){
+            data = {
+                specialization1: spec1
+            }
+        }
+        else{
+            data = {
+                specialization1: spec1,
+                specialization2: spec2
+            }
+        }
+        props.sendData(data)
+    }
 
     return(
         <div>
@@ -24,7 +41,11 @@ const AddSpecialityButton = () => {
             </h1>
             {show && (
                 <div className="accordian-body">
-                    <CustomInputForAddFaculty/>
+                    <CustomInputForAddFaculty placeholder={"Введите название специализации"} value={spec1}  onChange={event => {
+                        setSpec1(event.target.value)
+                        sendData()
+                    }
+                    }/>
                     <h1 className="label">Добавить специализацию
                         <div className="accordian-header2" onClick={handleOpen2}>
                             <div className="sign">{show2 ? '-' : '+'}
@@ -33,7 +54,11 @@ const AddSpecialityButton = () => {
                     </h1>
                     {show2 && (
                         <div className="accordian-body2">
-                            <CustomInputForAddFaculty placeholder={"Введите название факультета"} />
+                            <CustomInputForAddFaculty placeholder={"Введите название специализации"} value={spec2}  onChange={event => {
+                                setSpec2(event.target.value)
+                                sendData()
+                            }
+                            }/>
                         </div>
 
                     )}
--- src/components/forms/add_specialization_form/AddSpecializationForm.jsx
+++ src/components/forms/add_specialization_form/AddSpecializationForm.jsx
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import AddUserButton from "../../buttons/add_user_button/AddUserButton";
+import CustomDropdown from "../../inputs/CustomDropdown";
+import api from "../../../api/Api";
+import CustomInputForAddUser from "../../inputs/CustomInputForAddUser";
+import {useNavigate} from "react-router-dom";
 
 const AddSpecializationForm = () => {
+  const[faculty, setFaculty] = React.useState('0');
+  const[options, setOptions] = useState([])
+  const [spec1, setSpec1] = useState("")
+  const navigate = useNavigate()
+
+  async function addSpecialization(){
+    let data = {
+      "name": spec1
+    }
+    await api.addSpecialization(data, faculty).then(() => {
+      alert("Специализация " + spec1 + " успешно добавлена")
+      navigate('/settings')
+    }).catch(error => {
+      api.addSpecialization(data, faculty)
+    })
+  }
+
+  async function getFaculties(){
+    let data = await api.getFaculties().catch(error => {
+      api.getFaculties()
+    })
+    let returned = []
+    data.data.forEach(faculty => {
+      returned.push({label: faculty.name, value: faculty.facultyId})
+    })
+    setOptions(returned)
+  }
+
+  useEffect(()=> {
+    getFaculties()
+  }, [])
+
+
+  const handleChange = (event) => {
+    setFaculty(event.target.value);
+  };
+
   return (
-    <div>
-      
+    <div className="auth-form-container1">
+      <h2 className="h2">Добавление специализации</h2>
+      <label className="label">Факультет</label>
+      <CustomDropdown placeholder={"Выберите факультет"} options={options} handleChange={event => {
+        handleChange(event)
+      }
+      }/>
+      <CustomInputForAddUser placeholder={"Введите логин"} value={spec1} onChange={event => {
+        setSpec1(event.target.value)
+      }
+      }/>
+      <AddUserButton text="Добавить специализацию" onClick={addSpecialization}/>
     </div>
   );
 };
