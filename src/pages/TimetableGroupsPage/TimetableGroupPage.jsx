54461977fa03a8b1add50fa28e7e52ced4743fed
Shreksatronik <a.ordina@g.nsu.ru>
2023-06-22 12:47:25 +0700

    Dynamic nav

--- src/pages/TimetableGroupsPage/TimetableGroupPage.jsx
+++ src/pages/TimetableGroupsPage/TimetableGroupPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import "./TimetableGroupPage.css"
+import {useParams} from "react-router-dom";
 import TGForm from "../../components/forms/TGForm/TGForm";
 
 const TimetableGroupPage = () => {
+    const param = useParams()
+    alert(param.faculty)
   return (
     <div className="TimetableGroupPage">
         <h1 className="w1">Выберите группу</h1>
