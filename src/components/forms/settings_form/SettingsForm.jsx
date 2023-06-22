4ada32160ad01c4291700d6ecad27a891dc23ada
Shreksatronik <a.ordina@g.nsu.ru>
2023-06-22 12:04:24 +0700

    Button go back

--- src/components/forms/settings_form/SettingsForm.jsx
+++ src/components/forms/settings_form/SettingsForm.jsx
@@ -16,6 +16,7 @@ import AddSubjButtonSettings from "../../buttons/ButtonsForSettings/AddSubjButto
 import AddStudentButtonSettings from "../../buttons/ButtonsForSettings/AddStudentButtonSettings";
 import AddTeacherButtonSettings from "../../buttons/ButtonsForSettings/AddTeacherButtonSettings";
 import AddLessonButtonSettings from "../../buttons/ButtonsForSettings/AddLessonButtonSettings";
+import GoBackButtonSettings from "../../buttons/go_back_button/GoBackButtonSettings";
 
 const SettingsForm = () => {
   const navigate = useNavigate()
@@ -85,7 +86,7 @@ const SettingsForm = () => {
   return (
       <div className="settings-form-container">
           <div style={{transform: "translate(0%, -100%)"}}>
-            <GenerateButtonSettings text={"Назад"} onClick={openHome}/>
+            <GoBackButtonSettings text={"Назад"} onClick={openHome}/>
           </div>
         <label className="label1">Имя пользователя</label>
           <CustomInputForSettings name={cookies.getCookies("login")}/>
